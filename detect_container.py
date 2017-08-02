7a7d4f43fa9e5a26fe0e7385e7323f5b32fb783f
cleider <cleidergo@gmail.com>
2017-08-02 01:30:17 -0700

    swtiched to color tracking, blue cup now.

--- detect_container.py
+++ detect_container.py
@@ -1,15 +1,38 @@
 import cv2
+import numpy as np
+
 
 def main():
     capture()
+
+
+def images():
+    img = cv2.imread()
+
 
 def capture():
     cap = cv2.VideoCapture(0)
 
     while True:
+        # taking each frame
         ret, frame = cap.read()
 
-        cv2.imshow('frame', frame)
+        # converting BGR to HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+        # define range of blue color in HSV
+        lower_blue = np.array([51, 5, 71])
+        upper_blue = np.array([127, 255, 209])
+
+        # Threshold the HSV frames to get only blue colors
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+  
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+
+        cv2.imshow('mask', mask)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
